c02acc0911ceb2f31c9b109fcffa3104ac4d14de
Anastasia <winchester726@mail.ru>
2025-06-27 02:01:18 +0300

    Created MVP for clipboard tracking #1
    - I used the Toolkit class to get data from the clipboard;
    - Used try-catch for error handling;
    - I also used try-catch to exit the stream correctly when interrupted;

--- clipboard-collector/src/main/java/com/karmanov/tools/clipboarcollector/Main.java
+++ clipboard-collector/src/main/java/com/karmanov/tools/clipboarcollector/Main.java
@@ -1,11 +1,13 @@
 package com.karmanov.tools.clipboarcollector;
 
+import com.karmanov.tools.clipboarcollector.service.ClipBoardCollectorService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         SpringApplication.run(Main.class,args);
+        new ClipBoardCollectorService().getTextFromClipboard();
     }
 }
